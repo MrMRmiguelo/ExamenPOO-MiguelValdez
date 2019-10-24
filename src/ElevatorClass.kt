ca76d6519e75179eb34fb750dba59542a35e3f66
MrMRmiguelo <miguelovaldez01@gmail.com>
2019-10-23 19:59:48 -0600

    Creacion de clases

--- src/ElevatorClass.kt
+++ src/ElevatorClass.kt
@@ -1,4 +1,35 @@
-open class ElevatorClass (var piso: String ) {
+enum class status {
+    Detenido,Subir,Bajar
+}
+open class ElevatorClass (var piso: Int) {
+var estado: Int = 1
+    var elevador: Int = 0
+    var leer: Int = 0
+  open fun peticion()
+  {
+      println("Ingrese al piso que desea ir:")
+      leer = readLine()?.toInt() as Int
+      elevador == leer
+  }
+open fun EstadoElevador()
+{
+    peticion()
+    while(true)
+    {
+        if(leer > elevador)
+        {
+        elevadorsubir()
+        }
+        else if(leer < elevador)
+        {
+            elevadorbajar()
+        }
+        else if(leer == elevador)
+        {
+            detenido()
+        }
+    }
+}
 
 
     open fun show() {
@@ -6,10 +37,37 @@ open class ElevatorClass (var piso: String ) {
     }
     open fun detenido()
     {
-        println("El elevador de encuentra DETENIDO en el lobby")
+        println("El elevador ha llegado a su destino")
     }
-    open fun showelevador()
+    open fun elevadorbajar()
     {
-        println("El ")
+        println("Estado del elevador ABAJO, piso $estado")
+        while(estado != elevador) {
+            while (estado >= elevador) {
+                estado--
+                Thread.sleep(1000)
+                println("Estado del elevador ABAJO, piso $estado")
+
+            }
+        }
+        detenido()
+
     }
+open fun elevadorsubir()
+{
+
+        println("Estado del elevador ARRIBA, piso $estado")
+    while(estado != elevador
+        ) {
+        while (estado >= elevador) {
+            estado++
+            Thread.sleep(1000)
+            println("Estado del elevador ARRIBA, piso $estado")
+
+        }
+ detenido()
+    }
+}
+
+
 }
--- src/MainKT.kt
+++ src/MainKT.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
-
+    val Elevator = ElevatorClass(1)
+    Elevator.show()
+    Elevator.EstadoElevador()
+    print("----------------Gracias por usar nuestros servicios----------------")
 
 }
