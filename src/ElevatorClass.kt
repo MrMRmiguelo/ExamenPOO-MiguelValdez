926acd31071d8725140a374a0657814a519ad109
MrMRmiguelo <miguelovaldez01@gmail.com>
2019-10-23 18:25:30 -0600

    Creacion de clases

---
+++ src/ElevatorClass.kt
@@ -0,0 +1,15 @@
+open class ElevatorClass (var piso: String ) {
+
+
+    open fun show() {
+        println("\n El Piso en el que esta es $piso")
+    }
+    open fun detenido()
+    {
+        println("El elevador de encuentra DETENIDO en el lobby")
+    }
+    open fun showelevador()
+    {
+        println("El ")
+    }
+}
